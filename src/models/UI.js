7721e97b03c6cab28f3f252cb96c13004d92a406
Jormaedes Luis <jormahed@gmail.com>
2026-02-23 18:20:55 +0100

    toggle unidade de temperatura foi implementada

--- src/models/UI.js
+++ src/models/UI.js
@@ -7,12 +7,32 @@ class UI{
 		this.screenSearch = document.getElementById('screen-search');
 		this.screenWeatger = document.getElementById('screen-weather');
 		this.searchField = document.getElementById('search-field');
-
+		this.searchField2 = document.getElementById('search-field-2');
+		this.searchBtn2 = document.getElementById('search-btn-2');
+		this.btnCelsius = document.getElementById('btn-celsius');
+		this.btnFahrenheit = document.getElementById('btn-fahrenheit');
 	}
 
 
 	init (){
+		this.setUnitToggle();
 		this.setEvents();
+	}
+
+	setUnitToggle(){
+		this.btnCelsius.addEventListener('click', (e)=>{
+			if (this.btnCelsius.classList.contains('active'))
+				return ;
+			this.btnFahrenheit.classList.remove('active');
+			this.btnCelsius.classList.add('active')
+		})
+
+		this.btnFahrenheit.addEventListener('click', (e)=>{
+			if (this.btnFahrenheit.classList.contains('active'))
+				return ;
+			this.btnCelsius.classList.remove('active');
+			this.btnFahrenheit.classList.add('active')
+		})
 	}
 
 	setEvents() {
@@ -26,6 +46,8 @@ class UI{
 			console.log(response);
 		})
 	}
+
+
 }
 
 export default new UI();
