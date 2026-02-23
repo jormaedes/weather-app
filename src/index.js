8335523d935ccddbb110cb15da8e58361e6c3ba0
Jormaedes Luis <jormahed@gmail.com>
2026-02-23 16:37:10 +0100

    adicionei este codigo para testar a resposta da api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './styles/style.css';
+import WeatherAPI from './models/WeatherAPI';
+
+class App {
+	constructor() {
+		this.weatherAPI = new WeatherAPI();
+		this.response = null;
+	}
+
+	async getLocalData(local) {
+		this.response = await this.weatherAPI.getDates(local);
+		console.log(this.response);
+	}
+}
+
+const app = new App();
